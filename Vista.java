fa328a65b4ea9dbc5d9f39c7507c72baf4c4355e
Carlos May <carlosmay220@gmail.com>
2023-03-01 11:28:56 -0600

    Update Vista.java

--- Vista.java
+++ Vista.java
@@ -1,10 +1,7 @@
-import java.util.Arrays;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-import org.antlr.runtime.tree.ParseTree;
 import org.antlr.v4.gui.TreeViewer;
 
 public class Vista {
